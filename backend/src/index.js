1944710a794ee226cdbbc8da6d72c96e5cca9db3
XH1215 <chinxuanhong5328@gmail.com>
2024-10-03 16:46:08 +0800

    4:46

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3011;
 
 app.listen(port, () => {
     console.log(`Server is running on http://172.16.20.25:${port}`);
